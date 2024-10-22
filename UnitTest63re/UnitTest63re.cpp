a5d30c940c2d8c62e720e9336bd94fd5d0b655cc
Medliarskyi <med.uras111@gmail.com>
2024-10-22 22:10:24 +0300

    first master commit

---
+++ UnitTest63re/UnitTest63re.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab63re/lab63re.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest63re
+{
+	TEST_CLASS(UnitTest63re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int intArr[] = { 3, -1, 5, -10, 7, 0, -2, 8 };
+			int intSize = sizeof(intArr) / sizeof(intArr[0]);
+			int expectedSum = 23; 
+
+			Assert::AreEqual(expectedSum, sumPositiveElementsRec(intArr, intSize));
+		}
+	};
+}
---
+++ lab63re/lab63re.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Шаблонна функція для виведення масиву
+template <typename T>
+void printArray(T arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(5) << arr[i];
+    }
+    cout << endl;
+}
+
+// Шаблонна рекурсивна функція для обчислення суми додатних елементів
+template <typename T>
+T sumPositiveElementsRec(T arr[], int size, int index = 0) {
+    if (index == size) {
+        return 0;
+    }
+    T current = arr[index] > 0 ? arr[index] : 0;
+    return current + sumPositiveElementsRec(arr, size, index + 1);
+}
+
+int main() {
+    // Створимо масив типу int
+    int intArr[] = { 3, -1, 5, -10, 7, 0, -2, 8 };
+    int intSize = sizeof(intArr) / sizeof(intArr[0]);
+
+    // Виводимо масив типу int
+    cout << "Array int: ";
+    printArray(intArr, intSize);
+
+    // Обчислюємо суму додатних елементів для int рекурсивно
+    int intSumRec = sumPositiveElementsRec(intArr, intSize);
+    cout << "Sum of positive elements (int, recursive): " << intSumRec << endl;
+
+    // Створимо масив типу double
+    double doubleArr[] = { 3.5, -1.2, 5.1, -10.0, 7.8, 0.0, -2.3, 8.9 };
+    int doubleSize = sizeof(doubleArr) / sizeof(doubleArr[0]);
+
+    // Виводимо масив типу double
+    cout << "Array double: ";
+    printArray(doubleArr, doubleSize);
+
+    // Обчислюємо суму додатних елементів для double рекурсивно
+    double doubleSumRec = sumPositiveElementsRec(doubleArr, doubleSize);
+    cout << "Sum of positive elements (double, recursively): " << doubleSumRec << endl;
+
+    return 0;
+}
